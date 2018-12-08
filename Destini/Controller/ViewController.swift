13ea064c0d99d04d4adcaac1022a50477b06fb60
Hansa Anuradha <hansaanuradha@Hansas-MacBook-Pro.local>
2018-12-08 14:22:29 +0530

    Created the logic to get the current state of the user in the game, and then update the story

--- Destini/Controller/ViewController.swift
+++ Destini/Controller/ViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Create a variable to store the story number which user is currently in
+    var storyNumber : Int = 1
+    // Create a variable to store the story index which user is currently in
+    var storyIndex : Int = 0
     // Create a StoryBank object
-    let storyList = StoryBank()
+    let story = StoryBank()
     
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var topButton: UIButton!
@@ -19,17 +23,67 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set Top Button text to story1/answerA
-        
+        // Set the Initial status of the game --- Story1----
+        updateUI(sNumber: storyNumber)
     }
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        if sender.tag == 1 {
-            print("Top Button Pressed")
-        } else if sender.tag == 2 {
-            print("Bottom Button Pressed")
-        }
+        // Check answer
+        storyNumber = checkAnswer(sNumber: storyNumber, buttonTag: sender.tag)
+        // change the Story and Asnwers in UI
+        updateUI(sNumber: storyNumber)
     }
     
+    // Check answer
+    func checkAnswer(sNumber : Int, buttonTag : Int) -> Int {
+        // If user is in Story 1 and clicks answerA, then update the Story to story3
+        if sNumber == 1 &&  buttonTag == 1{
+            storyNumber = 3
+        }
+        // If user is in Story 1 and clicks answerB, then update the Story to story2
+        else if sNumber == 1 && buttonTag == 2 {
+            storyNumber = 2
+        }
+        // If user is in Story 3 and clicks answerA, then update the Story to story6
+        else if sNumber == 3 && buttonTag == 1 {
+            storyNumber = 6
+        }
+        // If user is in Story 3 and clicks answerB, then update the Story to story5
+        else if sNumber == 3 && buttonTag == 2 {
+            storyNumber = 5
+        }
+        // If user is in Story 2 and clicks answerA, then update the Story to story3
+        else if sNumber == 2 && buttonTag == 1 {
+            storyNumber = 3
+        }
+        // If user is in Story 2 and clicks answerB, then update the Story to story4
+        else if sNumber == 2 && buttonTag == 2 {
+            storyNumber = 4
+        }
+        
+        // Retun current story number
+        return storyNumber
+    }
+    
+    // Update Story and Answers in UI
+    func updateUI(sNumber : Int) {
+        
+        // Initialize the story index according to the value of story number
+        storyIndex = sNumber - 1
+
+        // When User clicks story/answer, then your storyLabel should change to story, topButton Title to story/answerA, and bottomButton Title to story/answerB
+        storyLabel.text = story.list[storyIndex].storyText
+        topButton.setTitle(story.list[storyIndex].answerA, for: .normal)
+        bottomButton.setTitle(story.list[storyIndex].answerB, for: .normal)
+        
+        // If answerA is empty, then hide the TopButton
+        if story.list[storyIndex].answerA == "" {
+            topButton.isHidden = true
+        }
+        // If answerB is empty, then hide the BottomButton
+        if story.list[storyIndex].answerB == "" {
+            bottomButton.isHidden = true
+        }
+    }
 }
 
--- Destini/Model/StoryBank.swift
+++ Destini/Model/StoryBank.swift
@@ -16,13 +16,26 @@ class StoryBank{
     init() {
         
         // Add stories one by one to the list
+        // Story 1 -----Index 0
         list.append(Story(story: "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\".", firstAnswer: "I\'ll hop in. Thanks for the help!", secondAnswer: "Better ask him if he\'s a murderer first."))
+        
+        // Story 2 -----Index 1
         list.append(Story(story: "He nods slowly, unphased by the question.", firstAnswer: "At least he\'s honest. I\'ll climb in.", secondAnswer: "Wait, I know how to change a tire."))
+        
+        // Story 3 -----Index 2
         list.append(Story(story: "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box.", firstAnswer: "I love Elton John! Hand him the cassette tape.", secondAnswer: "It\'s him or me! You take the knife and stab him."))
+        
+        // Story 4 -----Index 3
         list.append(Story(story: "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?", firstAnswer: "", secondAnswer: ""))
+        
+        // Story 5 -----Index 4
         list.append(Story(story: "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.", firstAnswer: "", secondAnswer: ""))
+            
+        // Story 6 -----Index 5
         list.append(Story(story: "You bond with the murderer while crooning verses of \"Can you feel the love tonight\". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: \"Try the pier.\"", firstAnswer: "", secondAnswer: ""))
         
     }
 }
+
+
 
